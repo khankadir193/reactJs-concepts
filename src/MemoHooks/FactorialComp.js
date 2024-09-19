387fe799735de0eba6dd1df4602e2f28935b8b84
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-19 13:23:00 +0530

    factorial calculation alongwith useMemo hooks

--- src/MemoHooks/FactorialComp.js
+++ src/MemoHooks/FactorialComp.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import ExpensiveComp from './ExpensiveComp';
 
-// const  ExpensiveCalculation = (factorial)=>{
-//     console.log('nnnnnn....',factorial);
-//     // const factorialRes = fatorialComputation(n);
-//     // console.log('ExpensiveComp',factorialRes);
-// }
+const  ExpensiveCalculation = ({ n })=>{
+    console.log('nnnnnn....',n);
+    const factorialRes = useMemo( ()=> fatorialComputation(n),[n]);
+    console.log('ExpensiveComp',factorialRes);
+    return factorialRes;
+}
 
-// const fatorialComputation = (n)=>{
-//     let res = 1;
-//     for(let i=2;i<=n;i++){
-//         res = res * i;
-//     }
-//     return res;
-// }
+const fatorialComputation = (n)=>{
+    let res = 1;
+    for(let i=2;i<=n;i++){
+        res = res * i;
+    }
+    return res;
+}
 
 const FactorialComp = () => {
     const [counter, setCounter] = React.useState(0);
-    const [factorial, setFactorial] = React.useState(5);
+    const [factorial, setFactorial] = React.useState(7);
 
     return (
         <div style={{ textAlign: 'center' }}>
@@ -25,8 +26,10 @@ const FactorialComp = () => {
                 <h2>{counter}</h2>
                 <button onClick={() => setCounter(counter + 1)}>counter</button>
             </div>
-
-            <ExpensiveComp n={factorial} />
+            {/* without useMemo */}
+            {/* <ExpensiveComp n={factorial} /> */}
+            {/* withuseMemo */}
+            <ExpensiveCalculation n={factorial} />
         </div>
     )
 }
