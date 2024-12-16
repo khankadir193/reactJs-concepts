86aa7608c501fe904f9873cf5bca197e2b720106
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-12-16 10:59:33 +0530

    updatedUser

--- src/crudApp/CrudComp.js
+++ src/crudApp/CrudComp.js
@@ -6,26 +6,27 @@ const CrudComp = () => {
     { id: 2, name: "Jane Smith", email: "jane@example.com" }]);
 
     const [newUser, setNewUser] = useState({ name: '', email: '' });
-    const [editing, setEditingUser] = useState(null);
+    const [editingUser, setEditingUser] = useState(null);
 
     const handleAddUser = (event) => {
         if (newUser.name && newUser.email) {
             const newId = users.length > 0 ? users[users.length - 1].id + 1 : 1;
-            setUsers([...users,{id:newId,...newUser}]);
-            setNewUser({name:'',email:''});
+            setUsers([...users, { id: newId, ...newUser }]);
+            setNewUser({ name: '', email: '' });
         }
     };
 
-    const handleEdit = () => {
-
-    };
+    const handleUpdateUser = ()=>{
+        const updatedUser = users.map((user)=> user.id === editingUser.id ? editingUser : user);
+        setUsers(updatedUser);
+    }
 
 
     const handleDelete = (removeId) => {
-        console.log('id...delete...',removeId);
-        console.log('users....',users);
-        const newData = users.filter((usr)=> usr.id !== removeId);
-        console.log('newData....',newData);
+        console.log('id...delete...', removeId);
+        console.log('users....', users);
+        const newData = users.filter((usr) => usr.id !== removeId);
+        console.log('newData....', newData);
         setUsers(newData);
     };
 
@@ -45,6 +46,7 @@ const CrudComp = () => {
                 />
                 <button onClick={handleAddUser}>Add</button>
             </div>
+            {/* display user */}
             <div>
                 <h3>UserList</h3>
                 {
@@ -55,10 +57,10 @@ const CrudComp = () => {
                             {
                                 users?.map((user) => {
                                     return (
-                                        <li style={{ display: 'flex', gap: '2vw', alignItems: 'center', justifyContent: 'center' }}>
+                                        <li key={user.id} style={{ display: 'flex', gap: '2vw', alignItems: 'center', justifyContent: 'center' }}>
                                             <span>{user.name} ({user.email})</span>
-                                            <button onClick={handleEdit}>Edit</button>
-                                            <button onClick={()=> handleDelete(user.id)}>Delete</button>
+                                            <button onClick={()=>setEditingUser(user)}>Edit</button>
+                                            <button onClick={() => handleDelete(user.id)}>Delete</button>
                                         </li>
                                     )
                                 })
@@ -67,6 +69,17 @@ const CrudComp = () => {
                     )
                 }
             </div>
+
+            {/* editing user */}
+            {editingUser && (<div>
+                <h3>Edit User</h3>
+                <div>
+                    <input type='text' placeholder='edit user' value={editingUser?.name} onChange={(e) => setEditingUser({ ...editingUser, name: e.target.value })} />
+                    <input type='email' placeholder='edit email' value={editingUser?.email} onChange={(e) => setEditingUser({ ...editingUser, email: e.target.value })} />
+                    <button onClick={handleUpdateUser}>Update</button>
+                    <button onClick={()=> setEditingUser(null)}>Cancel</button>
+                </div>
+            </div>)}
         </div>
     )
 }
