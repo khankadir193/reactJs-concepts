3dd325a932bee557cbf2929e3ad76535d10df0c6
Abdul Kadir Khan <abdul.khan@fugetroncorp.com>
2025-02-24 10:26:58 +0530

    autocomplete

--- src/AutoCompleteSearchBar/AutoComplete.js
+++ src/AutoCompleteSearchBar/AutoComplete.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import './autoComplete.css';
+import "./autoComplete.css";
 
 const AutoComplete = () => {
-  const [data, setData] = useState(0);
+  const [data, setData] = useState([]);
+  const [searchItem, setSearchItem] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch("https://dummyjson.com/recipes/search?q=Margherita");
+      const result = await fetch(
+        "https://dummyjson.com/recipes/search?q=Margherita"
+      );
       const res = await result.json();
-      setData(res);
+      setData(res.recipes || []);
       console.log("response...", res);
     };
 
@@ -19,10 +22,27 @@ const AutoComplete = () => {
     };
   }, [0]);
 
+  // Filter data based on search term
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(searchItem.toLowerCase())
+  );
+
   return (
     <div className="auto-complete-container">
       {/* <h2>{JSON.stringify(data)}</h2> */}
-      <input type="search" placeholder="search for item" />
+      <input
+        type="search"
+        placeholder="search for item"
+        onChange={(e) => setSearchItem(e.target.value)}
+      />
+
+      {
+        filteredData.length > 0 ? (
+            filteredData.map((item)=> <span>{item.name}</span>)
+        ) : (
+            <span>Search Not Found</span>
+        )
+      }
     </div>
   );
 };
