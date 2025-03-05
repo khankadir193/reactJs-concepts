5f405cd443263e4520ef6042cce1b5b5b17aa80e
Abdul Kadir Khan <abdul.khan@fugetroncorp.com>
2025-03-05 22:21:42 +0530

    file component

---
+++ src/FileStructure/FileComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import dataJson from './data.json';
+
+const FileComponent = () => {
+
+  return (
+    <div>
+        <h2>File Structure</h2>
+
+    </div>
+  );
+}
+
+export default FileComponent;
